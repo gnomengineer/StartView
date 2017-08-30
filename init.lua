3f842f757f25f50f56942b0dbcf53a4e2be9caec
troopa <troopa.der.pate@gmail.com>
2017-08-30 10:41:05 +0200

    a version showing 3 imageboxes with a cairo surface as image

--- init.lua
+++ init.lua
@@ -5,8 +5,6 @@ local wibox = require("wibox")
 local naughty = require("naughty")
 local gears = require("gears")
 
-local surface = cairo.ImageSurface(cairo.Format.RGB24,20,20)
-local cr = cairo.Context(surface)
 
 local startview_box = wibox({ 
     width =  screen[mouse.screen].geometry.width, 
@@ -15,7 +13,7 @@ local startview_box = wibox({
 
 local settings = {
     box_opacity = 0.6,
-    box_bg = "#000000",
+    box_bg = "#ff0000",
     starter_font = {"sans","italic","normal"},
     starter_font_size = 0.8,
     starter_font_color = "#ffffff"
@@ -23,35 +21,49 @@ local settings = {
 
 local starterList = {{"test","testIcon"}}
 startview_box.opacity = settings.box_opacity
+startview_box.bg = settings.box_bg 
 startview_box.ontop = true
 startview_box.visible = false
 local widget_container = wibox.widget {
     forced_num_cols = 5,
     horizontal_homogeneous = true,
+    spacing = 5,
     expand = false,
     layout = wibox.layout.grid
 }
 
-local function toggle_startview() 
+local function debug(val) 
+    naughty.notify({
+        text= val .. ""
+    })
+end
 
+local function toggle_startview() 
     startview_box.visible = not startview_box.visible
 end
 
 local function setup()
-    for i = 1, #starterList do
-        naughty.notify({
-            text= i .. "in list"
-        })
-        starter_widget = wibox.widget.textclock()
-        
+
+    surface = cairo.ImageSurface(cairo.Format.RGB24,100,100)
+    cr = cairo.Context(surface)
+    for i = 1, 3 do
+        --cr:set_source_surface(gears.surface.load("~/pictures/VennPurpose.VennPurpose.jpg"),0,0)
+        --cr:show_text("test")
+        cr:set_source(gears.color("#ffffff"))
+        cr:move_to(2,2)
+        cr:show_text("test")
+        cr:stroke()
+        --cr:paint()
+            
+        starter_widget = wibox.widget.imagebox(surface,false)
+        --starter_widget.forced_height =100
+        --starter_widget.forced_width = 100
         -- add widget to the main container
         widget_container:add(starter_widget)
     end
 
     startview_box:set_widget(widget_container)
 end
-
-
 
 return {
     toggle_startview = toggle_startview,
