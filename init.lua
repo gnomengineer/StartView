26442f69c9f8db2f076dcfe1ca8de8898f12fb74
troopa <troopa.der.pate@gmail.com>
2017-08-30 08:48:19 +0200

    added a simple textclock widget to test the positioning of widget

--- init.lua
+++ init.lua
@@ -22,6 +22,7 @@ local settings = {
 }
 
 local starterList = {{"test","testIcon"}}
+startview_box.opacity = settings.box_opacity
 startview_box.ontop = true
 startview_box.visible = false
 local widget_container = wibox.widget {
@@ -32,78 +33,16 @@ local widget_container = wibox.widget {
 }
 
 local function toggle_startview() 
-    for i = 1, #starterList do
-        starter_widget = wibox.widget.base.make_widget()
-        
-        starter_widget.draw = function(starter_widget, startview_box, cr, width, height)
-            --Icon
-            local icon = gears.surface(gears.surface.load(starterList[i].icon))
 
-            local tx, ty, sx, sy = 2,2,0.5,0.5
-            cr:translate(tx,ty)
-            cr:scale(sx,sy)
-            cr:set_source_surface(icon, 0, 0)
-            cr:paint()
-            cr:scale(1/sx,1/sy)
-            cr:translate(-tx,-ty)
-
-            --Titles
-            cr:select_font_face(unpack(settings.starter_font))
-            cr:set_font_face(cr:get_font_face())
-            cr:set_font_size(settings.starter_font_size)
-
-            text = starterList[i].text
-            text_width = cr:text_extents(text).width
-            text_height = cr:text_extents(text).height
-
-            cr:move_to(0,12)
-            cr:show_text(text)
-            cr:stroke()
-        end
-        
-        -- add widget to the main container
-        widget_container:add(starter_widget)
-    end
-
-    startview_box:set_widget(widget_container)
-   startview_box.visible = not startview_box.visible
+    startview_box.visible = not startview_box.visible
 end
 
 local function setup()
-    naughty.notify({
-        preset = naughty.config.presets.normal,
-        text = os.date("%H:%M \n %a, %d. %b %Y"),
-        position = "top_middle",
-    })
- 
     for i = 1, #starterList do
-        starter_widget = wibox.widget.base.make_widget()
-        
-        starter_widget.draw = function(starter_widget, startview_box, cr, width, height)
-            --Icon
-            local icon = gears.surface(gears.surface.load(starterList[i].icon))
-
-            local tx, ty, sx, sy = 2,2,0.5,0.5
-            cr:translate(tx,ty)
-            cr:scale(sx,sy)
-            cr:set_source_surface(icon, 0, 0)
-            cr:paint()
-            cr:scale(1/sx,1/sy)
-            cr:translate(-tx,-ty)
-
-            --Titles
-            cr:select_font_face(unpack(settings.starter_font))
-            cr:set_font_face(cr:get_font_face())
-            cr:set_font_size(settings.starter_font_size)
-
-            text = starterList[i].text
-            text_width = cr:text_extents(text).width
-            text_height = cr:text_extents(text).height
-
-            cr:move_to(0,12)
-            cr:show_text(text)
-            cr:stroke()
-        end
+        naughty.notify({
+            text= i .. "in list"
+        })
+        starter_widget = wibox.widget.textclock()
         
         -- add widget to the main container
         widget_container:add(starter_widget)
