68926a4d74b3dc181aba5bbf4e5a74cc1f352bc0
Daniel Foehn <dfoehn@thermoplan.ch>
2019-02-25 13:43:56 +0100

    make the layout more dynamic to screen and widget size

--- init.lua
+++ init.lua
@@ -4,6 +4,8 @@ local screen = screen
 local wibox = require("wibox")
 local naughty = require("naughty")
 local gears = require("gears")
+local filesystem = require("gears.filesystem")
+local image_path = filesystem.get_configuration_dir() .. "startview/"
 
 local surface = cairo.ImageSurface(cairo.Format.RGB24,20,20)
 local cr = cairo.Context(surface)
@@ -12,6 +14,8 @@ local source = string.sub(debug.getinfo(1,'S').source, 2)
 local path = string.sub(source, 1, string.find(source, "/[^/]*$"))
 local noicon = path .. "testIcon.png"
 local margin = 20
+local starter_width = 150
+local starter_height = 180
 --]]
 local settings = {
     box_opacity = 0.6,
@@ -27,26 +31,15 @@ local function printDebugInfo(val)
     })
 end
 
-local startview_box = wibox({
-    width =  screen[mouse.screen].geometry.width - 2*margin,
-    height = screen[mouse.screen].geometry.height - 2*margin,
-    x = margin,
-    y = margin,
-    opacity = settings.box_opacity,
-    shape = gears.shape.rounded_rect,
-    bg = settings.box_bg,
-    ontop = true,
-    visible = false
-})
 
 local starter_widgets = {}
 local starterList = {{"test","testIcon"}}
 local layout = wibox.layout.fixed.horizontal()
 
-for counter=0,15 do
+for counter=0,14 do
     starter_widget = wibox({
-        width = startview_box.width / 4 - 1.25 * margin,
-        height = startview_box.height / 4 - 1.25 * margin,
+        width = starter_width,
+        height = starter_height,
         bg = "#ff00ff",
         shape = gears.shape.rounded_rect,
         ontop = true,
@@ -54,29 +47,58 @@ for counter=0,15 do
     })
     local text = wibox.widget.textbox()
     text:set_text("Test")
+    text:set_valign("bottom")
+    text:set_align("center")
+    text:set_font("sans 14")
+
     local image = wibox.widget.imagebox()
-    image:set_image("test.png")
+    image:set_image(image_path .. "test.png")
+    image:set_forced_height(150)
+    image:set_forced_width(150)
+    local container = wibox.container.margin(image, 10, 10)
+
     local layout_starter_widget = wibox.layout.fixed.vertical()
+    layout_starter_widget:add(container)
     layout_starter_widget:add(text)
-    layout_starter_widget:add(image)
     starter_widget:set_widget(layout_starter_widget)
     starter_widgets[counter] = starter_widget
 end
+
+local count,rest = math.modf(#starter_widgets / 5)
+if rest > 0 then
+    rest = 1
+end
+
+local startview_box_width =  (5*margin + starter_width) * 5 + 5*margin
+local startview_box_height = (2*margin + starter_height) * (count + rest) + 2*margin
+
+local startview_box = wibox({
+    width = startview_box_width,
+    height = startview_box_height,
+    y = (screen[mouse.screen].geometry.height - startview_box_height) / 2,
+    x = (screen[mouse.screen].geometry.width - startview_box_width) / 2,
+    opacity = settings.box_opacity,
+    shape = gears.shape.rounded_rect,
+    bg = settings.box_bg,
+    ontop = true,
+    visible = false
+})
+
 
 local ycounter = 0
 local xcounter = 1
 
 for counter=0, #starter_widgets do
-    if counter % 4 == 0 then
+    if counter % 5 == 0 then
         ycounter = ycounter + 1
         xcounter = 1
     end
 
-    starter_widgets[counter].x = xcounter * margin + (xcounter - 1) * starter_widgets[counter].width + margin
-    starter_widgets[counter].y = ycounter * margin + (ycounter - 1) * starter_widgets[counter].height + margin
+    starter_widgets[counter].x = xcounter * (5 * margin) + (xcounter - 1) * starter_width + startview_box.x
+    starter_widgets[counter].y = ycounter * (2 * margin) + (ycounter - 1) * starter_height + startview_box.y
     xcounter = xcounter + 1
 end
-
+    
 local function toggle_startview()
     startview_box.visible = not startview_box.visible
     for counter = 0, #starter_widgets do
